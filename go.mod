bb5cdb069871fc1c8b2fbaa9e0ce304f4d999374
FelipeSoft <felipedecastrolima2@gmail.com>
2024-11-30 20:20:55 -0300

    [tada] First commit.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/FelipeSoft/go-secure-backup
+
+go 1.23.3
---
+++ internal/http/process.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("HTTP Server is running on port XXXX")
+}
